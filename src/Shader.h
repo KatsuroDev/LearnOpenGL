bb2346f831278ab3aa0a45b65f22738430e74b87
KatsuroDev <katsuro.userg@gmail.com>
2020-02-12 18:01:04 -0500

    Added functions to set vec2, vec3, vec4, mat2, mat3, mat4.

--- src/Shader.h
+++ src/Shader.h
@@ -2,6 +2,7 @@
 #define SHADER_H
 
 #include <GL/glew.h>
+#include <GL/glm/glm.hpp>
 
 #include <iostream>
 #include <fstream>
@@ -22,6 +23,21 @@ public:
   void SetBool(const std::string &name, bool value) const;
   void SetInt(const std::string &name, int value) const;
   void SetFloat(const std::string &name, float value) const;
+// ------------------------------------------------------------------------
+  void setVec2(const std::string &name, const glm::vec2 &value) const;
+  void setVec2(const std::string &name, float x, float y) const;
+// ------------------------------------------------------------------------
+  void setVec3(const std::string &name, const glm::vec3 &value) const;
+  void setVec3(const std::string &name, float x, float y, float z) const;
+// ------------------------------------------------------------------------
+  void setVec4(const std::string &name, const glm::vec4 &value) const;
+  void setVec4(const std::string &name, float x, float y, float z, float w) const;
+// ------------------------------------------------------------------------
+  void setMat2(const std::string &name, const glm::mat2 &mat) const;
+// ------------------------------------------------------------------------
+  void setMat3(const std::string &name, const glm::mat3 &mat) const;
+// ------------------------------------------------------------------------
+  void setMat4(const std::string &name, const glm::mat4 &mat) const;
 private:
   void CheckCompileErrors(unsigned int shader, std::string type);
 };
